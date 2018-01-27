cfe57774e4931a4d5d44953ebffb3d34b6566161
Eric Adamski <er.adamski@gmail.com>
2018-01-27 13:20:54 -0500

    :sparkles: (Main) Add tests and change logic
    closes #1. Only send selected code or the current line.

---
+++ spec/url-generator-spec.js
@@ -0,0 +1,35 @@
+'use babel';
+
+import { range } from 'ramda';
+
+import { CARBON_URL, getCarbonURL } from '../lib/carbon.now.sh';
+
+describe('.getCarbonURL', () => {
+    it('should be a function with arity 1', () => {
+        // Assert
+        expect(typeof(getCarbonURL)).toBe('function');
+        expect(getCarbonURL.length).toBe(1);
+    });
+
+    it('should return the CARBON_URL concat\'d with the code', () => {
+        // Arrange
+        const code = 'var test;';
+
+        // Act
+        const result = getCarbonURL(code);
+
+        // Assert
+        expect(result).toEqual(`${CARBON_URL}${encodeURI(code)}`);
+    });
+
+    it('should return undefined if code is longer than 500 chars', () => {
+        // Arrange
+        const code = range(0, 500).map(i => `${i}`).join(' ');
+
+        // Act
+        const result = getCarbonURL(code);
+
+        // Assert
+        expect(result).toBeUndefined();
+    });
+});

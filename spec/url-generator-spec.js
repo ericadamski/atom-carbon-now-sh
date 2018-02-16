6ce129590465c877438e87ee477dc224e51206f3
Eric Adamski <er.adamski@gmail.com>
2018-02-16 12:07:08 -0500

    closes #2, fixed url safety of the # character

--- spec/url-generator-spec.js
+++ spec/url-generator-spec.js
@@ -1,35 +1,55 @@
-'use babel';
+"use babel";
 
-import { range } from 'ramda';
+import { range } from "ramda";
 
-import { CARBON_URL, getCarbonURL } from '../lib/generate-url';
+import { CARBON_URL, getCarbonURL } from "../lib/generate-url";
 
-describe('.getCarbonURL', () => {
-    it('should be a function with arity 1', () => {
-        // Assert
-        expect(typeof(getCarbonURL)).toBe('function');
-        expect(getCarbonURL.length).toBe(1);
-    });
+describe(".getCarbonURL", () => {
+  it("should be a function with arity 1", () => {
+    // Assert
+    expect(typeof getCarbonURL).toBe("function");
+    expect(getCarbonURL.length).toBe(1);
+  });
 
-    it('should return the CARBON_URL concat\'d with the code', () => {
-        // Arrange
-        const code = '  var test;  ';
+  it("should return the CARBON_URL concat'd with the code", () => {
+    // Arrange
+    const code = "  var test;  ";
 
-        // Act
-        const result = getCarbonURL(code);
+    // Act
+    const result = getCarbonURL(code);
 
-        // Assert
-        expect(result).toEqual(`${CARBON_URL}${encodeURI(code.trim())}`);
-    });
+    // Assert
+    expect(result).toEqual(`${CARBON_URL}${encodeURI(code.trim())}`);
+  });
 
-    it('should return undefined if code is longer than 500 chars', () => {
-        // Arrange
-        const code = range(0, 500).map(i => `${i}`).join(' ');
+  it("should return undefined if code is longer than 500 chars", () => {
+    // Arrange
+    const code = range(0, 500)
+      .map(i => `${i}`)
+      .join(" ");
 
-        // Act
-        const result = getCarbonURL(code);
+    // Act
+    const result = getCarbonURL(code);
 
-        // Assert
-        expect(result).toBeUndefined();
-    });
+    // Assert
+    expect(result).toBeUndefined();
+  });
+
+  it("should replace the literall `#` char with `%23`", () => {
+    // Arrange
+    const code = `
+        def hello
+            #this is my code
+            1+1
+        end
+    `;
+
+    // Act
+    const result = getCarbonURL(code);
+
+    // Assert
+    expect(result).toEqual(
+      `${CARBON_URL}${encodeURI(code.trim()).replace("#", "%23")}`
+    );
+  });
 });
